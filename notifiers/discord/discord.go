9b378733213c7431ee3a1077a6dd47c7de48d6a9
ReeceTrx <harrynreece@yahoo.co.uk>
2025-09-27 04:31:48 +0100

    Update discord.go

--- notifiers/discord/discord.go
+++ notifiers/discord/discord.go
@@ -1,79 +1,39 @@
 package discord
 
 import (
-	"bl-shifts/notifiers"
+	"bytes"
 	"encoding/json"
-	"fmt"
 	"net/http"
-	"strings"
 )
 
-type DiscordNotifier struct {
+type Notifier struct {
 	WebhookURL string
 }
 
-type DiscordWebhookMessage struct {
-	Embed []DiscordWebhookEmbed `json:"embeds"`
-}
-
-type DiscordWebhookEmbed struct {
-	Title       string                `json:"title"`
-	Description string                `json:"description"`
-	Fields      []DiscordWebhookField `json:"fields"`
-}
-
-type DiscordWebhookField struct {
-	Name   string `json:"name"`
-	Value  string `json:"value"`
-	Inline bool   `json:"inline"`
-}
-
-// NewNotifier creates a new DiscordNotifier with the given webhook URL.
-func NewNotifier(webhookURL string) notifiers.Notifier {
-	return &DiscordNotifier{
+func NewNotifier(webhookURL string) *Notifier {
+	return &Notifier{
 		WebhookURL: webhookURL,
 	}
 }
 
-// Send sends the given shift codes to the Discord webhook in batches of 10.
-func (d *DiscordNotifier) Send(codes []string) error {
-	batchSize := 10
-	for i := 0; i < len(codes); i += batchSize {
-		end := min(i+batchSize, len(codes))
-		batch := codes[i:end]
-		message := DiscordWebhookMessage{
-			Embed: []DiscordWebhookEmbed{
-				{
-					Title:       "New Shift Codes",
-					Description: "Here are the latest shift codes, redeem at https://shift.gearboxsoftware.com/rewards",
-					Fields:      []DiscordWebhookField{},
-				},
-			},
-		}
-		for _, code := range batch {
-			message.Embed[0].Fields = append(message.Embed[0].Fields, DiscordWebhookField{
-				Name:   "Code",
-				Value:  code,
-				Inline: false,
-			})
-		}
-		payload, err := json.Marshal(message)
+// Send sends the message array to Discord
+func (d *Notifier) Send(messages []string) error {
+	for _, msg := range messages {
+		payload := map[string]string{"content": msg} // Only send your message; no test line
+		body, _ := json.Marshal(payload)
+
+		req, err := http.NewRequest("POST", d.WebhookURL, bytes.NewBuffer(body))
 		if err != nil {
-			return fmt.Errorf("failed to marshal message: %w", err)
-		}
-		req, err := http.NewRequest(http.MethodPost, d.WebhookURL, strings.NewReader(string(payload)))
-		if err != nil {
-			return fmt.Errorf("failed to create request: %w", err)
+			return err
 		}
 		req.Header.Set("Content-Type", "application/json")
-		resp, err := http.DefaultClient.Do(req)
+
+		client := &http.Client{}
+		resp, err := client.Do(req)
 		if err != nil {
-			return fmt.Errorf("failed to send request: %w", err)
+			return err
 		}
-		defer resp.Body.Close()
-		if resp.StatusCode != http.StatusOK && resp.StatusCode != http.StatusNoContent {
-			return fmt.Errorf("unexpected status code: %d", resp.StatusCode)
-		}
+		resp.Body.Close()
 	}
 	return nil
 }
