9048d3d55d1d2eaaeb55b2991e21e84f86f5681b
ReeceTrx <harrynreece@yahoo.co.uk>
2025-09-27 03:46:23 +0100

    Update retrievers.go

--- retrievers/retrievers.go
+++ retrievers/retrievers.go
@@ -1,6 +1,5 @@
 package retrievers
 
-// Retriever interface now returns codes, post timestamp, and error
 type Retriever interface {
-	GetCodes() ([]string, float64, error)
+	GetCodes() ([]string, float64, string, error)
 }
