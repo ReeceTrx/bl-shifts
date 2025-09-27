d3690d0816acda92febcf13ccfcaf5fd4ec4a340
ReeceTrx <harrynreece@yahoo.co.uk>
2025-09-27 02:51:45 +0100

    Update retrievers.go

--- retrievers/retrievers.go
+++ retrievers/retrievers.go
@@ -1,3 +1,7 @@
+package retrievers
+
+// Retriever is an interface for fetching shift codes from any source
 type Retriever interface {
+	// GetCodes returns the latest codes, the post timestamp, and an error if any
 	GetCodes() ([]string, float64, error)
 }
